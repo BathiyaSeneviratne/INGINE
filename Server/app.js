a5254aedd2d6446846c34ec8e3e56428b429fa8d
Bathiya Seneviratne <seneviratne.bathiya@gmail.com>
2019-09-13 22:12:30 +0530

    app.js updated

--- Server/app.js
+++ Server/app.js
@@ -10,12 +10,14 @@ const wifi = require('./routes/wifi');
 const graphics = require('./routes/graphics');
 const os = require('./routes/osInformation');
 
+
+
 //Middleware
-app.use('/cpu',cpu);
-app.use('/battery',battery);
-app.use('/wifi',wifi);
-app.use('/graphics',graphics);
-app.use('/os',os);
+app.use('/cpu', cpu);
+app.use('/battery', battery);
+app.use('/wifi', wifi);
+app.use('/graphics', graphics);
+app.use('/os', os);
 
 const PORT = 3000;
 app.listen(PORT, () => {
